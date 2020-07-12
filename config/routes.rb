85077ef1a71fcf6e5ab2f7045f03d980ae75fd89
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-07-12 05:04:40 +0000

    [add]some script files

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {   
   registrations: 'users/registrations',
   sessions: 'users/sessions' }
+
+  root "homes#top"
   
   namespace :user do
   	resource :users
