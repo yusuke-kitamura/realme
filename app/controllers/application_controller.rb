4dc67f0a75ab5bf49d194e16e21c7ea955d96d68
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-07-13 02:29:53 +0000

    [add]path after singin and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,24 @@ class ApplicationController < ActionController::Base
 
 	protected
 
+	def after_sign_in_path_for(resource)
+    	case resource
+    	when Salon
+      		owner_salons_path
+    	when User
+      		user_salons_path
+    	end
+  	end
+
+  	def after_sign_out_path_for(resource)
+    	case resource
+    	when :salon
+      		root_path
+    	when :user
+      		root_path
+    	end
+  	end
+
 	def configure_permitted_parameters
 		if resource_class == User
    			devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:age,:email,:job])
