e7926eb3fe303b941d6f7b93482f714eb6988151
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-07-15 14:08:17 +0000

    [add]to find salon id

--- app/controllers/salon/salons_controller.rb
+++ app/controllers/salon/salons_controller.rb
@@ -1,10 +1,15 @@
 class Salon::SalonsController < ApplicationController
 	def show
+		@salon = Salon.find(params[:id])
 	end
 
 	def edit
+		@salon = Salon.find(params[:id])
 	end
 
 	def update
+		@salon = Salon.find(params[:id])
+		@salon.update
+		redirect_to edit_user_users_path
 	end
 end
