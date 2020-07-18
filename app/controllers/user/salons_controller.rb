6c1fd95c4f856b2e818fa033123b3bf2b2ddfd12
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-07-18 15:45:13 +0000

    [remove]dont need

--- app/controllers/user/salons_controller.rb
+++ app/controllers/user/salons_controller.rb
@@ -7,9 +7,4 @@ class User::SalonsController < ApplicationController
 	def show
 		@salon = Salon.find(params[:id])
 	end
-
-	private
-	def salon_params
-		params.require(:salon).permit(:salon_name, :person_name,:number,:price,:purpose)
-	end
 end
