cabd82e2bd980b7b2364066d06666f279f4a7615
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-07-20 15:14:11 +0000

    [add]image_tag for salon

--- app/controllers/salon/salons_controller.rb
+++ app/controllers/salon/salons_controller.rb
@@ -10,7 +10,7 @@ class Salon::SalonsController < ApplicationController
 	def update
 		@salon = current_salon
 		@salon.update(salon_params)
-		redirect_to edit_user_users_path
+		redirect_to salon_salon_path
 	end
 
 	private
