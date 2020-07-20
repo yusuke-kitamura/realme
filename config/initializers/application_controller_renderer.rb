cd89398814e1e5ceb6f7bb616b2ad25c2fdb03ec
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-07-20 08:58:27 +0000

    [add]key to show profile image

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '87cabe62aa73b9be8400897769c3f1e4f052db1c05557633d1ba3c4464be08c4d7d4643e280349d1e8036491eff614920884e5530028beac7c7bf53b608db945'
