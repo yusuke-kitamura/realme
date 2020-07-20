7c4ec5ba6c12f8e28af77bace52dc19e61f7e74a
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-07-20 15:29:46 +0000

    [update]configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,11 +26,11 @@ class ApplicationController < ActionController::Base
 		if resource_class == User
    			devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:age,:email,:job])
    			devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
-   			devise_parameter_sanitizer.permit(:account_update,keys:[:name,:age,:email,:job])
+   			devise_parameter_sanitizer.permit(:account_update,keys:[:name,:age,:email,:job,:image])
 		elsif resource_class == Salon  
 		    devise_parameter_sanitizer.permit(:sign_up, keys: [:salon_name,:email,:person_name,:number,:price,:purpose])
    			devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
-   			devise_parameter_sanitizer.permit(:account_update,keys:[:salon_name,:email,:person_name,:number,:price,:purpose])
+   			devise_parameter_sanitizer.permit(:account_update,keys:[:salon_name,:email,:person_name,:number,:price,:purpose,:image])
     else
     		super
     end
