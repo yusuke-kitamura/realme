ff72ef4b6d780abc3cfdae3d59db0d63d70f22da
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-08-06 14:42:41 +0000

    [add]partial for user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
    			devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:age,:email,:job])
    			devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
    			devise_parameter_sanitizer.permit(:account_update,keys:[:name,:age,:email,:job,:image])
-		elsif resource_class == Salon  
+		elsif resource_class == Salon
 		    devise_parameter_sanitizer.permit(:sign_up, keys: [:salon_name,:email,:person_name,:number,:price,:purpose])
    			devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
    			devise_parameter_sanitizer.permit(:account_update,keys:[:salon_name,:email,:person_name,:number,:price,:purpose,:image])
